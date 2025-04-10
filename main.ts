9b3063c407e546acd3f4fcc4440c46a02b0c3a87
Manu <manukrishnaos45@gmail.com>
2025-04-11 02:45:28 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import { serveFile } from "https://deno.land/std@0.181.0/http/file_server.ts";
 
 
-Deno.serve((request: Request) => {
-  return await serveFile(request, 'index.html');
-});
+Deno.serve(async (request: Request) => { return await serveFile(request, 'index.html') });
 
